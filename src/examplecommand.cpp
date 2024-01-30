c52564718992a1e70c9cafad6d74018a04e5f887
Michael Fisher <mfisher@lvtk.org>
2024-01-29 23:34:32 -0500

    destroy all unused files

--- src/examplecommand.cpp
+++
@@ -1,8 +0,0 @@
-
-#include "examplecommand.hpp"
-
-ExampleCommand::ExampleCommand (ExampleSubsystem* subsystem)
-    : m_subsystem { subsystem } {
-    // Register that this command requires the subsystem.
-    AddRequirements (m_subsystem);
-}
