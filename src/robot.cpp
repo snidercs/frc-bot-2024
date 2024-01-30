a2a3dd0b21469d19ebaeaedb61d0b9e31464440a
Michael Fisher <mfisher@lvtk.org>
2024-01-29 20:27:19 -0500

    Remove testing code.

--- src/robot.cpp
+++ src/robot.cpp
@@ -7,7 +7,6 @@ void Robot::RobotInit() {
     m_chooser.SetDefaultOption (kAutoNameDefault, kAutoNameDefault);
     m_chooser.AddOption (kAutoNameCustom, kAutoNameCustom);
     frc::SmartDashboard::PutData ("Auto Modes", &m_chooser);
-    // frc::SmartDashboard::PutString("")
 }
 
 /**
