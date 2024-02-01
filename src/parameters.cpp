ad21bbdae5c9b61bb7e849b09095b7a2b743856a
Michael Fisher <mfisher@lvtk.org>
2024-02-01 10:22:15 -0500

    housekeeping: no magic numbers.

--- src/parameters.cpp
+++ src/parameters.cpp
@@ -1,19 +1,25 @@
 
 #include "parameters.hpp"
 
+#ifndef BOT_LOG_AXES
+#    define BOT_LOG_AXES 0
+#endif
+
 using snider::BotMode;
 
 void Parameters::process (const Context& context) noexcept {
     values = context;
-#if 0
+
+#if BOT_LOG_AXES
     for (int i = 0; i < 6; ++i)
-        if (_lastContext.axis[i] != context.axis[i])
+        if (lastContext.axis[i] != context.axis[i])
             std::clog << "[bot] axis #" << i << " = " << context.axis[i] << std::endl;
 
     for (int i = 0; i < 1; ++i)
-        if (_lastContext.povs[i] != context.povs[i])
+        if (lastContext.povs[i] != context.povs[i])
             std::clog << "[bot] dpad #" << i << " = " << context.povs[i] << std::endl;
 #endif
+
     // Save the context in the previous one for change detection and future
     // interpolations...
     lastContext = values;
