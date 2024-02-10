e5bd394d7602782a93bf16d2785f94ac74c82db0
Michael Fisher <mfisher@lvtk.org>
2024-02-10 11:05:13 -0500

    ports: add namespace containing well defined port and channel indexes.

---
+++ src/ports.hpp
@@ -0,0 +1,27 @@
+#pragma once
+
+namespace Port {
+/** The default gamepad port to use. */
+static constexpr int DefaultGamepad = 0;
+
+/** Default joystick port, not currently used. */
+static constexpr int DefaultJoystick = 1;
+
+/** Controller for the left drive motors. */
+static constexpr int DriveLeftLeader = 0;
+
+/** Controller for the right drive motors. */
+static constexpr int DriveRightLeader = 1;
+
+/** Controller for the shooter intake. */
+static constexpr int ShooterIntake = 2;
+
+/** Controller for the shooting. */
+static constexpr int ShooterShoot = 3;
+
+/** Controller for arm rotation. */
+static constexpr int ArmRotate = 4;
+
+/** Controller for arm pickup action. */
+static constexpr int ArmPickup = 5;
+} // namespace Port
