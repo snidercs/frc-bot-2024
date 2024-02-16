97fa5b0b6ae0e96ba3c9bfe0bbb6f9c5feb4e3fa
Michael Fisher <mfisher@lvtk.org>
2024-02-16 18:04:05 -0500

    add placeholders for mechanical arm and shooter. Utilize new parameter accessors.

---
+++ src/mechanicalarm.hpp
@@ -0,0 +1,33 @@
+#pragma once
+
+#include <frc/motorcontrol/PWMSparkMax.h>
+
+#include "ports.hpp"
+#include "snider/console.hpp"
+
+class MechanicalArm {
+public:
+    MechanicalArm()  = default;
+    ~MechanicalArm() = default;
+
+    enum Direction { Up,
+                     Down };
+
+    void move (Direction direction) {
+        switch (direction) {
+            case Up:
+                snider::console::log ("Push arm away");
+                break;
+            case Down:
+                snider::console::log ("Pull arm close");
+                break;
+        }
+    }
+
+    void moveUp() { move (Up); }
+    void moveDown() { move (Down); }
+
+private:
+    frc::PWMSparkMax intakeMotor { Port::ShooterIntake };
+    frc::PWMSparkMax shootMotor { Port::ShooterIntake };
+};
