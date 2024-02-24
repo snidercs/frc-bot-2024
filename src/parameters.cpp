4178062c7d8bc889c447b56d050193b2393d7e4b
Michael Fisher <mfisher@lvtk.org>
2024-02-24 14:20:12 -0500

    enable axis logging

--- src/parameters.cpp
+++ src/parameters.cpp
@@ -5,7 +5,7 @@
 #include "parameters.hpp"
 
 #ifndef BOT_LOG_AXES
-#    define BOT_LOG_AXES 0
+#    define BOT_LOG_AXES 1
 #endif
 
 using snider::BotMode;
@@ -25,12 +25,12 @@ void Parameters::process (const Context& context) noexcept {
 
 #if BOT_LOG_AXES
     for (int i = 0; i < 6; ++i)
-        if (lastContext.axis[i] != context.axis[i])
-            std::clog << "[bot] axis #" << i << " = " << context.axis[i] << std::endl;
+        if (lastContext.axis[i] != values.axis[i])
+            std::clog << "[bot] axis #" << i << " = " << values.axis[i] << std::endl;
 
     for (int i = 0; i < 1; ++i)
-        if (lastContext.povs[i] != context.povs[i])
-            std::clog << "[bot] dpad #" << i << " = " << context.povs[i] << std::endl;
+        if (lastContext.povs[i] != values.povs[i])
+            std::clog << "[bot] dpad #" << i << " = " << values.povs[i] << std::endl;
 #endif
 
     // Save the context in the previous one for change detection and future
