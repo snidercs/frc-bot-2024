a8921884042c7ad563ba669e3ba96598090e5aff
Michael Fisher <mfisher@lvtk.org>
2024-02-28 07:36:31 -0500

    shooter: docs and logging

--- src/shooter.hpp
+++ src/shooter.hpp
@@ -8,28 +8,51 @@
 #include "ports.hpp"
 #include "snider/console.hpp"
 
+/** The shooter interface.
+
+    Controls motors used in loading notes and shooting
+    them.  It operates as a state machine with three states: Idle, Shooting, 
+    Loading. Idle can transition to Shooting or Loading then back.  Shooting and 
+    Loading do not transition directly to each other which prevents shoot/load 
+    overlaps.
+*/
 class Shooter {
 public:
     Shooter() {
+        reset();
         bottomMotor.Follow (topMotor, false);
         for (auto* const mt : motors) {
             mt->Set (0.0);
+            mt->SetClosedLoopRampRate (0.125);
+            mt->SetOpenLoopRampRate (0.125);
+            mt->ClearFaults();
+            mt->SetIdleMode (IdleMode::kBrake);
         }
     }
 
     ~Shooter() {}
 
+    /** State of this shooter. */
     enum State {
         Idle,     ///> Idle state. not shooting. not loading.
         Shooting, ///> Is shooting.
         Loading   ///> is Loading.
     };
 
+    /** Reset to default values. */
+    void reset() {
+        _state = lastState = Idle;
+        loadSpeed          = -0.20;
+        loadDurationMs     = 600;
+        shootSpeed         = 1.0;
+        shootDurationMs    = 1000;
+        periodMs           = 20;
+    }
+
     /** Prepare motors to load if not loading or shooting. */
     void load() {
         if (_state != Idle)
             return;
-        std::clog << "[bot] prepare to load\n";
         _state = Loading;
         tick   = std::max (5, loadDurationMs / periodMs);
     }
@@ -38,7 +61,6 @@ public:
     void shoot() {
         if (_state != Idle)
             return;
-        std::clog << "[bot] prepare to shoot\n";
         _state = Shooting;
         tick   = std::max (5, shootDurationMs / periodMs);
     }
@@ -48,29 +70,30 @@ public:
         _state = Idle;
     }
 
+    /** Returns the current state. */
     constexpr State state() const noexcept { return _state; }
-
+    /** Returns true if in Idle state. */
     constexpr bool isIdle() const noexcept { return _state == Idle; }
+    /** Returns true if in Loading state. */
     constexpr bool isLoading() const noexcept { return _state == Loading; }
+    /** Returns true if in Shooting state. */
     constexpr bool isShooting() const noexcept { return _state == Shooting; }
 
     /** Set appropriate motor speed and update state if needed. */
-    void process() {
+    void process() noexcept {
         // clang-format off
         const auto speed = _state == Shooting ? shootSpeed :
                            _state == Loading ? loadSpeed :
                            _state == Idle ? 0.0 : 0.0;
-        
+        // clang-format on
+
         if (_state != lastState && _state != Idle) {
             // if state changed and not idle....
             std::clog << "[bot] " << stateString() << std::endl;
         }
 
         // topMotor.Set (speed);
-        const auto volts = speed * 12.0;
-        topMotor.SetVoltage (units::volt_t { volts });
-
-        // clang-format on
+        topMotor.SetVoltage (units::volt_t { speed * 12.0 });
 
         if (! isIdle() && --tick <= 0) {
             std::clog << "[bot] shoot/load sequence finished.\n";
@@ -81,16 +104,18 @@ public:
     }
 
 private:
+    using IdleMode  = rev::CANSparkBase::IdleMode;
+    using MotorType = rev::CANSparkLowLevel::MotorType;
+
     State _state { Idle };
     State lastState { Idle };
     double loadSpeed { -0.20 };
+    int loadDurationMs { 600 };
     double shootSpeed { 1.0 };
     int shootDurationMs { 1000 };
-    int loadDurationMs { 600 };
     int periodMs { 20 };
     int tick = 0;
 
-    using MotorType = rev::CANSparkLowLevel::MotorType;
     rev::CANSparkMax bottomMotor { Port::BottomShootingWheel, MotorType::kBrushed };
     rev::CANSparkMax topMotor { Port::TopShootingWheel, MotorType::kBrushed };
     std::array<rev::CANSparkMax*, 2> motors { &topMotor, &bottomMotor };
