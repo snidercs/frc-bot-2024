c8e0bd9418626b04d9e37a48bda75e8f95704029
Michael Fisher <mfisher@lvtk.org>
2024-03-06 20:22:55 -0500

    types: add more aliases to frc chrono classes.

--- src/types.hpp
+++ src/types.hpp
@@ -5,3 +5,6 @@
 
 using IdleMode  = rev::CANSparkBase::IdleMode;
 using MotorType = rev::CANSparkLowLevel::MotorType;
+
+using MetersPerSecond  = units::meters_per_second_t;
+using RadiansPerSecond = units::radians_per_second_t;
