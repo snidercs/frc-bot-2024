99ace54ce105e8763add5d0ae2998c46c010832a
Michael Fisher <mfisher@lvtk.org>
2024-03-07 10:35:24 -0500

    tests: finish parameters tests.

--- test/parameterstest.cpp
+++ test/parameterstest.cpp
@@ -1,6 +1,6 @@
 
-#include <cstdlib>
 #include <cmath>
+#include <cstdlib>
 
 #include <gtest/gtest.h>
 
@@ -14,18 +14,18 @@ namespace detail {
     @param high The high limit inclusive.
     @return The random number.
 */
-template<typename T> 
+template <typename T>
 static T random (T low, T high) {
     if (low >= high)
         throw std::runtime_error ("random(): low is greater or equal to high");
 
-   return low + static_cast<T> (std::rand()) / (static_cast<T> (RAND_MAX / (high - low)));
+    return low + static_cast<T> (std::rand()) / (static_cast<T> (RAND_MAX / (high - low)));
 }
 
 /** Make a context with random values set. */
 static Parameters::Context randomContext() {
     Parameters::Context c;
-    
+
     // random joystick values
     for (auto& a : c.axis)
         a = detail::random (-1.0, 1.0);
@@ -38,7 +38,7 @@ static Parameters::Context randomContext() {
     for (auto a : c.axis)
         if (a != 0.0)
             ++numNonZero;
-    
+
     if (numNonZero <= 0)
         throw std::runtime_error ("randomContext(): didn't generate random numbers");
 
@@ -46,13 +46,10 @@ static Parameters::Context randomContext() {
 }
 
 static bool axisUsesDeadZone (int a) {
-    return a == Parameters::LeftStickX ||
-        a == Parameters::LeftStickY ||
-        a == Parameters::RightStickX ||
-        a == Parameters::RightStickY;
+    return a == Parameters::LeftStickX || a == Parameters::LeftStickY || a == Parameters::RightStickX || a == Parameters::RightStickY;
 }
 
-}
+} // namespace detail
 
 class ParametersTest : public testing::Test {
 protected:
@@ -67,20 +64,38 @@ TEST_F (ParametersTest, ResetClearsAll) {
     for (int b = 0; b < Parameters::MaxButtons; ++b)
         EXPECT_EQ (params.getButtonValue (b), false);
     for (int d = 0; d < Parameters::MaxPOVs; ++d)
-        EXPECT_EQ (params.getPOVValue(d), 0);
+        EXPECT_EQ (params.getPOVValue (d), 0);
 }
 
 TEST_F (ParametersTest, DeadZone) {
     const auto oc = detail::randomContext();
     params.reset();
     params.process (oc);
-    
+
     for (int a = 0; a < Parameters::MaxAxes; ++a) {
         const auto original = oc.axis[a];
-        const auto value = params.getAxisValue (a);
-        if (detail::axisUsesDeadZone(a) && std::abs (original) <= Parameters::DeadZone)
+        const auto value    = params.getAxisValue (a);
+        if (detail::axisUsesDeadZone (a) && std::abs (original) <= Parameters::DeadZone)
             EXPECT_EQ (value, 0.0);
         else
             EXPECT_EQ (value, original);
     }
+}
+
+TEST_F (ParametersTest, Helpers) {
+    const auto oc = detail::randomContext();
+    params.reset();
+    params.process (oc);
+
+    EXPECT_EQ (params.getButtonValue (Parameters::ButtonA), params.getAButton());
+    EXPECT_EQ (params.getButtonValue (Parameters::ButtonB), params.getBButton());
+    EXPECT_EQ (params.getButtonValue (Parameters::ButtonX), params.getXButton());
+    EXPECT_EQ (params.getButtonValue (Parameters::ButtonY), params.getYButton());
+
+    EXPECT_EQ (params.getAxisValue (Parameters::LeftStickX), params.getLeftStickX());
+    EXPECT_EQ (params.getAxisValue (Parameters::LeftStickY), params.getLeftStickY());
+    EXPECT_EQ (params.getAxisValue (Parameters::RightStickX), params.getRightStickX());
+    EXPECT_EQ (params.getAxisValue (Parameters::RightStickY), params.getRightStickY());
+    EXPECT_EQ (params.getAxisValue (Parameters::TriggerLeft), params.getTriggerLeft());
+    EXPECT_EQ (params.getAxisValue (Parameters::TriggerRight), params.getTriggerRight());
 }
