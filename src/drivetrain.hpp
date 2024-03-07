8c2d8d833bdc137e19e1d635690bb68bfff366c6
Michael Fisher <mfisher@lvtk.org>
2024-03-06 20:23:12 -0500

    drive: isolate simulation code into its own class. Closes #49

--- src/drivetrain.hpp
+++ src/drivetrain.hpp
@@ -5,10 +5,12 @@
 #pragma once
 
 #include <array>
+#include <memory>
 #include <numbers>
 
 #include <frc/AnalogGyro.h>
 #include <frc/Encoder.h>
+#include <frc/RobotController.h>
 #include <frc/controller/PIDController.h>
 #include <frc/controller/SimpleMotorFeedforward.h>
 #include <frc/kinematics/DifferentialDriveKinematics.h>
@@ -29,6 +31,7 @@
 #include <units/velocity.h>
 
 #include "ports.hpp"
+#include "types.hpp"
 
 /**
  * Represents a differential drive style drivetrain.
@@ -36,13 +39,15 @@
 class Drivetrain {
 public:
     Drivetrain();
-    ~Drivetrain() = default;
+    ~Drivetrain() {
+        simulation.reset();
+    }
 
-    static constexpr units::meters_per_second_t MaxSpeed =
-        3.0_mps; // 3 meters per second
-    static constexpr units::radians_per_second_t MaxAngularSpeed {
-        std::numbers::pi
-    }; // 1/2 rotation per second
+    /** 3 Meters per second */
+    static constexpr MetersPerSecond MaxSpeed = 3.0_mps;
+
+    /** 1/2 rotation per second */
+    static constexpr RadiansPerSecond MaxAngularSpeed { std::numbers::pi };
 
     static constexpr units::meter_t TrackWidth = 0.559_m;
     static constexpr double WheelRadius        = 0.076; // meters
@@ -53,6 +58,12 @@ public:
     void postProcess();
     void resetOdometry (const frc::Pose2d& pose);
     frc::Pose2d position2d() const { return odometry.GetPose(); }
+
+    void initializeSimulation() {
+        if (simulation != nullptr)
+            return;
+        simulation = std::make_unique<Simulation> (*this);
+    }
 
 private:
     using IdleMode  = rev::CANSparkMax::IdleMode;
@@ -79,22 +90,61 @@ private:
         units::meter_t { 0.0 }
     };
 
-    // Gains are for example purposes only - must be determined for your own
-    // robot!
+    // Gains are for example purposes only: must be determined for your own bot!
     frc::SimpleMotorFeedforward<units::meters> feedforward { 1_V, 3_V / 1_mps };
 
     //==========================================================================
-    // Simulation classes help us simulate our robot
-    frc::sim::AnalogGyroSim gyroSim { gyro };
-    frc::sim::EncoderSim leftEncoderSim { leftEncoder };
-    frc::sim::EncoderSim rightEncoderSim { rightEncoder };
-    frc::Field2d fieldSim;
-    frc::LinearSystem<2, 2, 2> drivetrainSystem =
-        frc::LinearSystemId::IdentifyDrivetrainSystem (
-            1.98_V / 1_mps, 0.2_V / 1_mps_sq, 1.5_V / 1_mps, 0.3_V / 1_mps_sq);
-    frc::sim::DifferentialDrivetrainSim drivetrainSimulator {
-        drivetrainSystem, TrackWidth, frc::DCMotor::CIM (2), 8, 2_in
+    class Simulation {
+    public:
+        Simulation() = delete;
+        Simulation (Drivetrain& o) : owner (o) {
+            frc::SmartDashboard::PutData ("Field", &fieldSim);
+        }
+
+        /** Update the overall state of the simulation. */
+        void update() {
+            // To update our simulation, we set motor voltage inputs, update the
+            // simulation, and write the simulated positions and velocities to our
+            // simulated encoder and gyro. We negate the right side so that positive
+            // voltages make the right side move forward.
+            drivetrainSimulator.SetInputs (
+                units::volt_t { owner.leftLeader.Get() } * frc::RobotController::GetInputVoltage(),
+                units::volt_t { owner.rightLeader.Get() } * frc::RobotController::GetInputVoltage());
+            drivetrainSimulator.Update (20_ms);
+
+            leftEncoderSim.SetDistance (drivetrainSimulator.GetLeftPosition().value());
+            leftEncoderSim.SetRate (drivetrainSimulator.GetLeftVelocity().value());
+            rightEncoderSim.SetDistance (
+                drivetrainSimulator.GetRightPosition().value());
+            rightEncoderSim.SetRate (drivetrainSimulator.GetRightVelocity().value());
+            gyroSim.SetAngle (-drivetrainSimulator.GetHeading().Degrees().value());
+        }
+
+        /** Called from the drive train during "RobotPeriodic" */
+        void onPostProcess() {
+            fieldSim.SetRobotPose (owner.odometry.GetPose());
+        }
+
+        /** Called when the drivetrain resets its Odometry. */
+        void onOdometryReset (const frc::Pose2d& pose) {
+            drivetrainSimulator.SetPose (pose);
+        }
+
+    private:
+        Drivetrain& owner;
+        frc::sim::AnalogGyroSim gyroSim { owner.gyro };
+        frc::sim::EncoderSim leftEncoderSim { owner.leftEncoder };
+        frc::sim::EncoderSim rightEncoderSim { owner.rightEncoder };
+        frc::Field2d fieldSim;
+        frc::LinearSystem<2, 2, 2> drivetrainSystem =
+            frc::LinearSystemId::IdentifyDrivetrainSystem (
+                1.98_V / 1_mps, 0.2_V / 1_mps_sq, 1.5_V / 1_mps, 0.3_V / 1_mps_sq);
+        frc::sim::DifferentialDrivetrainSim drivetrainSimulator {
+            drivetrainSystem, TrackWidth, frc::DCMotor::CIM (2), 8, 2_in
+        };
     };
+
+    std::unique_ptr<Simulation> simulation;
 
     void setSpeeds (const frc::DifferentialDriveWheelSpeeds& speeds);
     void updateOdometry();
