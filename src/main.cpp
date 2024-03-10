6a64dcbebf90b8468d7dd666aea3af1343303bed
Michael Fisher <mfisher@lvtk.org>
2024-03-10 15:21:36 -0400

    camera: add vision server. confirmed working.

--- src/main.cpp
+++ src/main.cpp
@@ -12,6 +12,11 @@
 #include <frc/smartdashboard/SmartDashboard.h>
 #include <frc/trajectory/TrajectoryGenerator.h>
 
+#include <opencv2/core/core.hpp>
+#include <opencv2/core/types.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <cameraserver/CameraServer.h>
+
 #include "snider/padmode.hpp"
 
 #include "drivetrain.hpp"
@@ -80,7 +85,7 @@ public:
             auto matchPos = lua::config::match_start_position();
             trajectory    = detail::makeTrajectory (matchPos);
         } catch (const std::exception& e) {
-            std::cerr 
+            std::cerr
                 << "[bot] error: lua trajectory could not be parsed." << std::endl
                 << "[bot] what: " << e.what() << std::endl
                 << "[bot] loading fallback trajectory" << std::endl;
@@ -93,6 +98,10 @@ public:
         }
 
         L.collect_garbage();
+#ifndef RUNNING_FRC_TESTS
+        std::thread vision (visionThread);
+        vision.detach();
+#endif
     }
 
     /** Called every 20 ms, no matter the mode. This RUNS AFTER the mode 
@@ -261,6 +270,41 @@ private:
         }
 
         params.process (ctx);
+    }
+
+    static void visionThread() {
+        // Get the USB camera from CameraServer
+        cs::UsbCamera camera = frc::CameraServer::StartAutomaticCapture();
+        // Set the resolution
+        camera.SetResolution (640, 480);
+
+        // Get a CvSink. This will capture Mats from the Camera
+        cs::CvSink cvSink = frc::CameraServer::GetVideo();
+        // Setup a CvSource. This will send images back to the Dashboard
+        cs::CvSource outputStream =
+            frc::CameraServer::PutVideo ("Rectangle", 640, 480);
+
+        // Mats are very memory expensive. Lets reuse this Mat.
+        cv::Mat mat;
+
+        while (true) {
+            // Tell the CvSink to grab a frame from the camera and
+            // put it
+            // in the source mat.  If there is an error notify the
+            // output.
+            if (cvSink.GrabFrame (mat) == 0) {
+                // Send the output the error.
+                outputStream.NotifyError (cvSink.GetError());
+                // skip the rest of the current iteration
+                continue;
+            }
+            // Put a rectangle on the image
+            rectangle (mat, cv::Point (100, 100), 
+                            cv::Point (400, 400), 
+                            cv::Scalar (255, 255, 255), 5);
+            // Give the output stream a new image to display
+            outputStream.PutFrame (mat);
+        }
     }
 };
 
