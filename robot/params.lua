a1e09251f4e6e51b868350af5a50b64f9f45d4bc
Michael Fisher <mfisher@lvtk.org>
2024-03-11 21:01:25 -0400

    lua: format the code.

--- robot/params.lua
+++ robot/params.lua
@@ -1,11 +1,11 @@
---- Access to bot Parameters.
---- @name params
+---Access to bot Parameters.
+---@name params
 local M = {}
 
---- Returns the speed ranged from  -1.0 to 1.0
+---Returns the speed ranged from  -1.0 to 1.0
 function M.speed() return cxx.params.speed() end
 
---- Returns rotation ranged from -1.0 to 1.0
+---Returns rotation ranged from -1.0 to 1.0
 function M.rotation() return cxx.params.rotation() end
 
 return M
--- robot/robot.lua
+++ robot/robot.lua
@@ -11,22 +11,22 @@ local drivetrain = cxx.drivetrain;
 ---@return boolean
 function M.shooting() return shooter.shooting() end
 
---- Returns true if the robot is loading.
---- @return boolean
+---Returns true if the robot is loading.
+---@return boolean
 function M.loading() return shooter.loading() end
 
---- Loads a note if not shooting or already loading.
+---Loads a note if not shooting or already loading.
 function M.load() return shooter.load() end
 
 --- Shoots a note if not loading and not already shooting.
 function M.shoot() return shooter.shoot() end
 
---- Trys to stop the shooter motors regardless of what they're doing.
+---Trys to stop the shooter motors regardless of what they're doing.
 function M.stop_shooter() return shooter.stop() end
 
---- Drive the robot.
---- @param speed number Speed ranged -1 to 1
---- @param rot number Rotation ranged -1 to 1
+---Drive the robot.
+---@param speed number Speed ranged -1 to 1
+---@param rot number Rotation ranged -1 to 1
 function M.drive(speed, rot)
     drivetrain.drive(speed, rot)
 end
