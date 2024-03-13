76361ac68952ff445d9c4101df8719e0cd858dea
Michael Fisher <mfisher@lvtk.org>
2024-03-13 12:37:02 -0400

    integration test: actually run the bot in unit tests auto/test modes supported.

--- test/enginetest.cpp
+++ test/enginetest.cpp
@@ -12,13 +12,40 @@ namespace config = lua::config;
 
 class EngineTest : public testing::Test {
 public:
-    EngineTest() {}
+    EngineTest() {
+        if (gTimedRobot != nullptr)
+            gTimedRobot->RobotInit();
+    }
 };
 
 TEST_F (EngineTest, Period) {
     EXPECT_NE(gTimedRobot, nullptr);
     double actual = units::time::millisecond_t (gTimedRobot->GetPeriod()).value();
     EXPECT_EQ (config::get ("engine", "period").as<double>(), actual);
+}
+
+TEST_F (EngineTest, RunAutonomous) {
+    // run the bot in test mode for one second.
+    gTimedRobot->AutonomousInit();
+    int period = std::max (1, static_cast<int> (units::time::millisecond_t (gTimedRobot->GetPeriod()).value()));
+    int ticks = 1000 / period;
+    while (--ticks >= 0) {
+        gTimedRobot->AutonomousPeriodic();
+        gTimedRobot->RobotPeriodic();
+    }
+    gTimedRobot->AutonomousExit();
+}
+
+TEST_F (EngineTest, RunTest) {
+    // run the bot in test mode for one second.
+    gTimedRobot->TestInit();
+    int period = std::max (1, static_cast<int> (units::time::millisecond_t (gTimedRobot->GetPeriod()).value()));
+    int ticks = 1000 / period;
+    while (--ticks >= 0) {
+        gTimedRobot->TestPeriodic();
+        gTimedRobot->RobotPeriodic();
+    }
+    gTimedRobot->TestExit();
 }
 
 TEST_F (EngineTest, RequireAll) {
