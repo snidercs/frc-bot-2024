917e2f23d4a7c668dfb9a56279391a446c746cca
Michael Fisher <mfisher@lvtk.org>
2024-03-17 18:37:50 -0400

    tidy readme & move sudocker.sh to util folder.

--- dockerbuild.sh
+++ dockerbuild.sh
@@ -1,4 +1,4 @@
 :
 
 # this is a fast build of the project without the overhead of watching the file system
-./sudocker.sh ./gradlew --no-watch-fs build
+./util/sudocker.sh ./gradlew --no-watch-fs build
