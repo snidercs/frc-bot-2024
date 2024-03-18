4baa111a46a1d245bbe90ca2c120c1757fed0110
Michael Fisher <mfisher@kushview.net>
2024-03-17 23:44:51 -0400

    luajit: msvc: build working.

--- util/build-luajit-msvc.sh
+++ util/build-luajit-msvc.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 set -e
+
 here="$(pwd)"
-prefix="$HOME/SDKs/botlib/msvc"
+prefix="${here}/vendordeps/sdk/msvc"
 ljpath="vendordeps/luajit"
 
-mkdir -p "$prefix/include" "$prefix/lib"
+mkdir -p "$prefix/include/luajit-2.1" "$prefix/lib"
 
 # "clean" msvc luajit build
 cd "${ljpath}" && \
@@ -12,3 +13,18 @@ cd "${ljpath}" && \
     git checkout src
 cd "${here}"
 
+set -x
+
+./util/build-luajit-msvc.bat
+
+cp -f "${ljpath}/src/lua51.lib" "${prefix}/lib/"
+headers="lua.h lualib.h luajit.h lua.hpp lauxlib.h luaconf.h"
+for f in $headers; do
+    cp -f "${ljpath}/src/${f}" "$prefix/include/luajit-2.1/"
+done
+
+# "clean" msvc luajit build
+cd "${ljpath}" && \
+    rm -rf src && \
+    git checkout src
+cd "${here}"
