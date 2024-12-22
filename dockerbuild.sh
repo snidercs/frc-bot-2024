16e8440f3c01e31dd28320d500976ea2fa40e515
Doug Horner <horner@mieweb.com>
2024-12-22 14:34:28 -0500

    Update dockerbuild.sh to build both luajit-linux64 AND roborio

--- dockerbuild.sh
+++ dockerbuild.sh
@@ -1,4 +1,4 @@
 :
 
 # this is a fast build of the project without the overhead of watching the file system
-./util/docker-run.sh ./gradlew --no-watch-fs build
+./util/docker-run.sh ./util/build-luajit-linux64.sh && ./util/build-luajit-roborio.sh && ./gradlew --no-watch-fs build
