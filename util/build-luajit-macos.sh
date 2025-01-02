179c7ddea36ca7c2a25c5ecd02dffd4b5db91b5b
Martin Carapia <pops123.fm@gmail.com>
2025-01-02 12:24:11 -0500

    fix: broken macos build scripts

--- util/build-luajit-macos.sh
+++ util/build-luajit-macos.sh
@@ -2,22 +2,38 @@
 
 export MACOSX_DEPLOYMENT_TARGET=10.12
 
-host_arch="$1"
-if [ -z "$host_arch" ]; then
-    host_arch=$(arch)
-fi
-
 ljdir="$(pwd)/vendordeps/luajit"
 sdkdir="$(pwd)/vendordeps/sdk"
 
 cd "$ljdir"
 set -ex
 
-### Build for host machine
+### Build for arm machine
 make clean
-make amalg CC="clang -arch $host_arch -std=c99" \
+make amalg CC="clang -arch arm64 -std=c99" \
     BUILDMODE="static" \
     XCFLAGS="-DLUAJIT_ENABLE_LUA52COMPAT=1" \
     PREFIX="${sdkdir}/macos"
 make install PREFIX="${sdkdir}/macos"
 make clean
+
+### Set arm .a file aside
+mv "${sdkdir}/macos/lib/libluajit-5.1.a" "${sdkdir}/macos/lib/libluajit-5.1-arm.a"
+
+### Build for x86_64 machine
+
+make clean
+make amalg CC="clang -arch x86_64 -std=c99" \
+    BUILDMODE="static" \
+    XCFLAGS="-DLUAJIT_ENABLE_LUA52COMPAT=1" \
+    PREFIX="${sdkdir}/macos"
+make install PREFIX="${sdkdir}/macos"
+make clean
+
+### Set x86_64 .a file aside
+mv "${sdkdir}/macos/lib/libluajit-5.1.a" "${sdkdir}/macos/lib/libluajit-5.1-x86_64.a"
+
+### Use lipo to combine the two builds
+lipo -create -output "${sdkdir}/macos/lib/libluajit-5.1.a" "${sdkdir}/macos/lib/libluajit-5.1-arm.a" "${sdkdir}/macos/lib/libluajit-5.1-x86_64.a"
+
+
